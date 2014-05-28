54c5855ee30713c6066dffd62b7e9d21296bdf0c
Adam Smith <adam.smith18@ntlworld.com>
2014-05-28 19:05:50 +0100

    Menu Added

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 adamjsmith
 ===============
-The wordpress theme for my personal site, a onepager using Bootstrap.
+The wordpress theme for my personal site, just a single page using Bootstrap, I'm not interesting enough to fill more than one page yet.
--- header.php
+++ header.php
@@ -8,24 +8,46 @@
 <!--[if !(IE 7) | !(IE 8) ]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
+<?php
+
+$navArgs = array(
+	'theme_location'  => 'navbar',
+	'menu'            => '',
+	'container'       => '',
+	'container_class' => '',
+	'container_id'    => '',
+	'menu_class'      => 'nav',
+	'menu_id'         => '',
+	'echo'            => true,
+	'fallback_cb'     => 'wp_page_menu',
+	'before'          => '',
+	'after'           => '',
+	'link_before'     => '<li>',
+	'link_after'      => '</li>',
+	'items_wrap'      => '',
+	'depth'           => 0,
+	'walker'          => ''
+);
+
+?>
+
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap.min.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css" />
 	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/bootstrap.min.js"></script>
 	<?php wp_head(); ?>
 </head>
 <header class="navbar navbar-fixed-top navbar-inverse">
 	<div class="navbar-inner">
-		<div id="container">
-			<div id="logo">
-				Adam Smith
-			</div>
-		</div>
+		<div class="container">
+			<div id="logo">Adam Smith</div>
+			<?php wp_nav_menu( array('menu_class' => 'nav', 'container' => 'false', 'menu_id' => 'nav',  'walker' =>  new T5_Nav_Menu_Walker_Simple) ); ?>
+		
 	</div>
 </header>
 <body>
